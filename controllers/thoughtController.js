3fe3202dd9fec0502154751a55b79764da92d524
ivetteramos <101986174+ivetteramos@users.noreply.github.com>
2022-08-17 18:22:35 -0700

    added new code and pushing to main

---
+++ controllers/thoughtController.js
@@ -0,0 +1,81 @@
+const { Thoughts, User } = require('../Modules');
+
+module.exports = {
+    getThoughts(req, res) {
+        Thoughts.find()
+            .then((thoughts) => res.json(thoughts))
+            .catch((err) => res.status(500).json(err));
+    },
+    getSingleThought(req, res) {
+        Thoughts.findOne({ _id: req.params.thoughtId })
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: 'No thought with that ID' })
+                    : res.json(thought)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+
+    // This creates a new application document from the models application.js application schema
+    // Creates a application in the USER document
+
+    createThought(req, res) {
+        Thought.create(req.body)
+            .then((thoughts) => {
+                return User.findOneAndUpdate(
+                    { _id: req.body.userId },
+                    { $addToSet: { thoughts: thoughtId } },
+                    { new: true }
+                );
+            })
+            .then((user) =>
+                !user
+                    ? res.status(404).json({
+                        message: 'Thought created, but found no user with that ID',
+                    })
+                    : res.json('Created the thought 🎉')
+            )
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
+
+    updateThought(req, res) {
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+            .then((thoughts) =>
+                !thoughts
+                    ? res.status(404).json({ message: 'No thought with this id!' })
+                    : res.json(thoughts)
+            )
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
+    //  it will find the application by ID and delete the  application with that ID. 
+    deleteThoughts(req, res) {
+        Thought.findOneAndRemove({ _id: req.params.thoughtId })
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: 'No thought with this id!' })
+                    : User.findOneAndUpdate(
+                        { thoughts: req.params.thougthId },
+                        { $pull: { thougt: req.params.thoughtId } },
+                        { new: true }
+                    )
+            )
+            .then((user) =>
+                !user
+                    ? res.status(404).json({
+                        message: 'Application created but no user with this id!',
+                    })
+                    : res.json({ message: 'Application successfully deleted!' })
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+};
